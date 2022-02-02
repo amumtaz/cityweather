58cef9d67f1ad73af85b0c1369f839f533e337e6
amumtaz <amumtaz@users.noreply.github.com>
2022-02-01 22:28:57 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests, json
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-API_KEY = "c9d0bde1af0ba1b92aa3f3a223ea04f3"
+API_KEY = "your personal api key goes here"
 
 def displayWeather(data,city):
        # getting the main dict block
